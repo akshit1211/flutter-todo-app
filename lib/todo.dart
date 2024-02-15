a58cafef05b5cd814b4ac0ededeb2de38b6cc7f2
akshit1211 <trehana12@gmail.com>
2024-02-15 15:48:27 +0530

    todo.g.dart

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,7 +1,18 @@
+import 'package:hive/hive.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'todo.g.dart';
+
+@JsonSerializable()
+@HiveType(typeId: 01)
 class Todo {
+  @HiveField(0)
   String? title;
+  @HiveField(1)
   String? id;
+  @HiveField(2)
   bool? isDone;
+  @HiveField(3)
   int? priority;
 
   Todo({
@@ -9,5 +20,7 @@ class Todo {
     this.id,
     this.isDone,
     this.priority,
-  })
+  });
 }
+
+//dart run build_runner watch --delete-conflicting-outputs
